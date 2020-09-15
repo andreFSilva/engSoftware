3951804fbdaff4417f11e4c41b3c2b298be1db5c
andrealekhine <andrealekhine@msn.com>
2020-09-15 20:41:47 -0300

    db postgresql

--- gestao/settings/base.py
+++ gestao/settings/base.py
@@ -80,12 +80,28 @@ WSGI_APPLICATION = 'gestao.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
+# db postgresql
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'engsoft',
+        'USER': 'postgres',
+        'PASSWORD': 'karpov10',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
 
 # Password validation
--- home/views.py
+++ home/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.contrib import auth
 
 
-
 def home(request):
     auth.logout(request)
     return render(request, 'home.html')
