364989e52d916c58b67024faa62321f5295ca346
andrealekhine <andrealekhine@msn.com>
2020-09-15 22:21:25 -0300

    teste

--- gestao/settings/heroku.py
+++ gestao/settings/heroku.py
@@ -1,14 +1,14 @@
 import environ
-from gestao.settings.base import *
+from gestao.settings.base import*
 
 
 env = environ.Env()
 
-DEBUG = env.bool('DEBUG', False)
+DEBUG = env.bool("DEBUG", False)
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 DATABASES = {
     'default': env.db(),
